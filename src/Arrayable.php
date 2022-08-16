6c5ee464e721375641a56a5630ee06a8c2236642
Son Truong <sontruong2509@gmail.com>
2022-08-16 17:29:45 +0700

    update path

--- src/Arrayable.php
+++ src/Arrayable.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace TysonLaravel\Enums\Traits;
+namespace TysonLaravel\Enums;
 
 trait Arrayable
 {
--- src/InvokableCases.php
+++ src/InvokableCases.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace TysonLaravel\Enums\Traits;
+namespace TysonLaravel\Enums;
 
 use TysonLaravel\Enums\Exceptions\UndefinedCaseError;
 
